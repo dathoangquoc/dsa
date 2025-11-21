7b9c008d3de7c4b301acf4cab9ee23897788d3ed
dathoangquoc <dathgqc@gmail.com>
2025-11-21 13:15:23 +0700

    celebrity problem

--- matrix/celebrityProblem.ts
+++ matrix/celebrityProblem.ts
@@ -31,4 +31,65 @@ Examples:
 
     Input: mat[][] = [[1]]
     Output: 0
- */
+*/
+
+class CelebrityProblem {
+    // if A knows B, A cannot be the celebrity, else B cannot be the celebrity
+    twoPointers(matrix: number[][]): number {
+        // valid matrix check
+        if (matrix.length === 0) return -1;
+
+        let res = -1;
+        let l = 0;
+        let r = matrix.length - 1;
+        
+        // narrow down to 1 person
+        while (l < r) {
+            // l knows r -> l cannot be a celeb
+            if (matrix[l][r] === 1) {
+                l++;
+            } else {
+                // l doesnt know r -> r cannot be a celeb
+                r--;
+            }
+        }
+
+        res = l;
+
+        // check the last person
+        for (let i = 0; i < matrix.length; i++) {
+            // if res knows someone OR someone does not know res
+            if ((matrix[res][i] === 1 && i !== res) || matrix[i][res] === 0) {
+                return -1;
+            }
+        }
+
+        return res
+    }
+}
+
+{
+    const s = new CelebrityProblem();
+    console.log(
+        s.twoPointers([
+            [1, 1, 0, 0], 
+            [0, 1, 0, 0], 
+            [0, 1, 1, 0],
+            [0, 1, 0 ,1]
+        ])
+    );  // 1
+    console.log(
+        s.twoPointers([
+            [1, 1, 0], 
+            [0, 1, 0], 
+            [0, 1, 1]
+        ])
+    );  // 1
+    console.log(
+        s.twoPointers([
+            [1, 1], 
+            [1, 1]
+        ])
+    );  // -1
+    console.log(s.twoPointers([[1]]));  // 0
+}
